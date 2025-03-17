491b47bc4167cacebecf345d134c8e66b0ee23d6
Solomiia-Hotska <solomiia.hotska.pm.2023.@lpnu.ua>
2025-03-17 12:02:52 +0200

    Add new pages and design this

--- reciclica-app/src/app/app-routing.module.ts
+++ reciclica-app/src/app/app-routing.module.ts
@@ -9,113 +9,200 @@ const routes: Routes = [
   },
   {
     path: 'splash-screen',
-    loadChildren: () => import('./pages/splash-screen/splash-screen.module').then(m => m.SplashScreenPageModule)
+    loadChildren: () =>
+      import('./pages/splash-screen/splash-screen.module').then(
+        (m) => m.SplashScreenPageModule
+      ),
   },
   {
     path: 'forgot-password-1',
-    loadChildren: () => import('./pages/forgot-password-1/forgot-password-1.module').then(m => m.ForgotPassword1PageModule)
+    loadChildren: () =>
+      import('./pages/forgot-password-1/forgot-password-1.module').then(
+        (m) => m.ForgotPassword1PageModule
+      ),
   },
   {
     path: 'create-account-1',
-    loadChildren: () => import('./pages/create-account-1/create-account-1.module').then(m => m.CreateAccount1PageModule),
-    data: { animation: 'none' }
+    loadChildren: () =>
+      import('./pages/create-account-1/create-account-1.module').then(
+        (m) => m.CreateAccount1PageModule
+      ),
+    data: { animation: 'none' },
   },
   {
     path: 'forgot-password-2',
-    loadChildren: () => import('./pages/forgot-password-2/forgot-password-2.module').then(m => m.ForgotPassword2PageModule)
+    loadChildren: () =>
+      import('./pages/forgot-password-2/forgot-password-2.module').then(
+        (m) => m.ForgotPassword2PageModule
+      ),
   },
   {
     path: 'forgot-password-3',
-    loadChildren: () => import('./pages/forgot-password-3/forgot-password-3.module').then(m => m.ForgotPassword3PageModule)
+    loadChildren: () =>
+      import('./pages/forgot-password-3/forgot-password-3.module').then(
+        (m) => m.ForgotPassword3PageModule
+      ),
   },
   {
     path: 'forgot-password-4',
-    loadChildren: () => import('./pages/forgot-password-4/forgot-password-4.module').then( m => m.ForgotPassword4PageModule)
+    loadChildren: () =>
+      import('./pages/forgot-password-4/forgot-password-4.module').then(
+        (m) => m.ForgotPassword4PageModule
+      ),
   },
   {
     path: 'confirm-1',
-    loadChildren: () => import('./pages/confirm-1/confirm-1.module').then( m => m.Confirm1PageModule)
+    loadChildren: () =>
+      import('./pages/confirm-1/confirm-1.module').then(
+        (m) => m.Confirm1PageModule
+      ),
   },
   {
     path: 'confirm-2',
-    loadChildren: () => import('./pages/confirm-2/confirm-2.module').then( m => m.Confirm2PageModule)
+    loadChildren: () =>
+      import('./pages/confirm-2/confirm-2.module').then(
+        (m) => m.Confirm2PageModule
+      ),
   },
   {
     path: 'sign-in-1',
-    loadChildren: () => import('./pages/sign-in-1/sign-in-1.module').then( m => m.SignIn1PageModule)
+    loadChildren: () =>
+      import('./pages/sign-in-1/sign-in-1.module').then(
+        (m) => m.SignIn1PageModule
+      ),
   },
   {
     path: 'sign-in-2',
-    loadChildren: () => import('./pages/sign-in-2/sign-in-2.module').then( m => m.SignIn2PageModule)
+    loadChildren: () =>
+      import('./pages/sign-in-2/sign-in-2.module').then(
+        (m) => m.SignIn2PageModule
+      ),
   },
   {
     path: 'create-account-2',
-    loadChildren: () => import('./pages/create-account-2/create-account-2.module').then( m => m.CreateAccount2PageModule)
+    loadChildren: () =>
+      import('./pages/create-account-2/create-account-2.module').then(
+        (m) => m.CreateAccount2PageModule
+      ),
   },
   {
     path: 'create-account-3',
-    loadChildren: () => import('./pages/create-account-3/create-account-3.module').then( m => m.CreateAccount3PageModule)
+    loadChildren: () =>
+      import('./pages/create-account-3/create-account-3.module').then(
+        (m) => m.CreateAccount3PageModule
+      ),
   },
   {
     path: 'homepage',
-    loadChildren: () => import('./pages/homepage/homepage.module').then( m => m.HomepagePageModule)
+    loadChildren: () =>
+      import('./pages/homepage/homepage.module').then(
+        (m) => m.HomepagePageModule
+      ),
   },
   {
     path: 'confirm-3',
-    loadChildren: () => import('./pages/confirm-3/confirm-3.module').then( m => m.Confirm3PageModule)
+    loadChildren: () =>
+      import('./pages/confirm-3/confirm-3.module').then(
+        (m) => m.Confirm3PageModule
+      ),
   },
   {
     path: 'history-of-donation',
-    loadChildren: () => import('./pages/history-of-donation/history-of-donation.module').then( m => m.HistoryOfDonationPageModule)
+    loadChildren: () =>
+      import('./pages/history-of-donation/history-of-donation.module').then(
+        (m) => m.HistoryOfDonationPageModule
+      ),
   },
   {
     path: 'detailed-info',
-    loadChildren: () => import('./pages/detailed-info/detailed-info.module').then( m => m.DetailedInfoPageModule)
+    loadChildren: () =>
+      import('./pages/detailed-info/detailed-info.module').then(
+        (m) => m.DetailedInfoPageModule
+      ),
   },
   {
     path: 'profile',
-    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+    loadChildren: () =>
+      import('./pages/profile/profile.module').then((m) => m.ProfilePageModule),
   },
   {
     path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomePageModule),
   },
   {
     path: 'notification',
-    loadChildren: () => import('./pages/notification/notification.module').then( m => m.NotificationPageModule)
+    loadChildren: () =>
+      import('./pages/notification/notification.module').then(
+        (m) => m.NotificationPageModule
+      ),
   },
   {
     path: 'usefull-information',
-    loadChildren: () => import('./pages/usefull-information/usefull-information.module').then( m => m.UsefullInformationPageModule)
+    loadChildren: () =>
+      import('./pages/usefull-information/usefull-information.module').then(
+        (m) => m.UsefullInformationPageModule
+      ),
   },
   {
     path: 'suportive-service',
-    loadChildren: () => import('./pages/suportive-service/suportive-service.module').then( m => m.SuportiveServicePageModule)
+    loadChildren: () =>
+      import('./pages/suportive-service/suportive-service.module').then(
+        (m) => m.SuportiveServicePageModule
+      ),
   },
   {
     path: 'walking-pet',
-    loadChildren: () => import('./pages/walking-pet/walking-pet.module').then( m => m.WalkingPetPageModule)
+    loadChildren: () =>
+      import('./pages/walking-pet/walking-pet.module').then(
+        (m) => m.WalkingPetPageModule
+      ),
   },
   {
     path: 'walking-pet-animal',
-    loadChildren: () => import('./pages/walking-pet-animal/walking-pet-animal.module').then( m => m.WalkingPetAnimalPageModule)
+    loadChildren: () =>
+      import('./pages/walking-pet-animal/walking-pet-animal.module').then(
+        (m) => m.WalkingPetAnimalPageModule
+      ),
   },
   {
     path: 'donate',
-    loadChildren: () => import('./pages/donate/donate.module').then( m => m.DonatePageModule)
+    loadChildren: () =>
+      import('./pages/donate/donate.module').then((m) => m.DonatePageModule),
   },
   {
     path: 'faq',
-    loadChildren: () => import('./pages/faq/faq.module').then( m => m.FaqPageModule)
+    loadChildren: () =>
+      import('./pages/faq/faq.module').then((m) => m.FaqPageModule),
   },
   {
     path: 'donate-for-pet',
-    loadChildren: () => import('./pages/donate-for-pet/donate-for-pet.module').then( m => m.DonateForPetPageModule)
+    loadChildren: () =>
+      import('./pages/donate-for-pet/donate-for-pet.module').then(
+        (m) => m.DonateForPetPageModule
+      ),
   },
   {
     path: 'donate-for-shelter',
-    loadChildren: () => import('./pages/donate-for-shelter/donate-for-shelter.module').then( m => m.DonateForShelterPageModule)
+    loadChildren: () =>
+      import('./pages/donate-for-shelter/donate-for-shelter.module').then(
+        (m) => m.DonateForShelterPageModule
+      ),
   },
+
+  {
+    path: 'payment',
+    loadChildren: () =>
+      import('./pages/payment/payment.module').then((m) => m.PaymentPageModule),
+  },  {
+    path: 'adoption',
+    loadChildren: () => import('./pages/adoption/adoption.module').then( m => m.AdoptionPageModule)
+  },
+  {
+    path: 'addnewcard',
+    loadChildren: () => import('./pages/addnewcard/addnewcard.module').then( m => m.AddnewcardPageModule)
+  },
+
 ];
 
 @NgModule({
@@ -124,4 +211,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ reciclica-app/src/app/pages/addnewcard/addnewcard.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-addnewcard',
+  templateUrl: './addnewcard.page.html',
+  styleUrls: ['./addnewcard.page.scss'],
+  imports: [IonicModule],
+})
+export class AddnewcardPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
