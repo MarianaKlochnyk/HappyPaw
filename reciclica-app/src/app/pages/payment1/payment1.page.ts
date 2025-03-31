1a215f95363ff7782227abb7acdb68d5f761c78b
Solomiia-Hotska <solomiia.hotska.pm.2023.@lpnu.ua>
2025-03-31 21:32:37 +0300

    final design

---
+++ reciclica-app/src/app/pages/payment1/payment1.page.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+@Component({
+  selector: 'app-payment1',
+  templateUrl: './payment1.page.html',
+  styleUrls: ['./payment1.page.scss'],
+  imports: [IonicModule],
+})
+export class Payment1Page implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
