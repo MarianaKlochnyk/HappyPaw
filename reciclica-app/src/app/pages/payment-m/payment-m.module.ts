e8bd381a9e1663df4182608f246db85210c6c690
olyapl <olha.pliatsko.pm.2023@lpnu.ua>
2025-04-02 10:47:49 +0300

    Add bd for loc and donate

---
+++ reciclica-app/src/app/pages/payment-m/payment-m.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { PaymentMPageRoutingModule } from './payment-m-routing.module';
+
+import { PaymentMPage } from './payment-m.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    PaymentMPageRoutingModule,
+    PaymentMPage,
+  ],
+  declarations: [],
+})
+export class PaymentMPageModule {}
