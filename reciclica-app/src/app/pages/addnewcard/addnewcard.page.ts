73c7149db98b52d864f638a1137cb8d2772690e7
Anastasia Teodorovska <anastasiia.teodorovska.pm.2023@lpnu.ua>
2025-04-08 13:52:34 +0300

    Menu bar updating

--- reciclica-app/src/app/pages/addnewcard/addnewcard.page.ts
+++ reciclica-app/src/app/pages/addnewcard/addnewcard.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-addnewcard',
@@ -8,7 +9,22 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule],
 })
 export class AddnewcardPage implements OnInit {
-  constructor() {}
-
+      constructor(private router: Router) { }
+    
+        goStatistic() {
+                this.router.navigate(['/statistics']);
+      }
+    
+      goHomepage() {
+              this.router.navigate(['/homepage']);
+      }
+      
+       goNotification() {
+              this.router.navigate(['/notification']);
+      }
+    
+      goLocation() {
+                  this.router.navigate(['/location']);
+        }
   ngOnInit() {}
 }
