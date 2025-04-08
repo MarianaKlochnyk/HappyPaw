a484fb01272c43daf857fb26f15ac52dcfe540f9
olyapl <olha.pliatsko.pm.2023@lpnu.ua>
2025-04-08 13:27:11 +0300

    Finish reading fom db

--- reciclica-app/src/app/pages/history-of-donation/history-of-donation.page.ts
+++ reciclica-app/src/app/pages/history-of-donation/history-of-donation.page.ts
@@ -18,22 +18,56 @@ import { SupabaseService } from 'src/service/supabase.service';
 })
 export class HistoryOfDonationPage implements OnInit {
   donations: any[] = []; // Масив для збереження донатів
+  shelter: any = null;
+ // shelter_name: any[] = [];
 
   constructor(private supabaseService: SupabaseService, private router: Router) {}
 
   async ngOnInit() {
-    // Завантажуємо донати, викликаючи асинхронний метод getDonations
     const { data, error } = await this.supabaseService.getDonations();
-
+  
     if (error) {
-      console.error('Помилка при отриманні донатів:', error);
-    } else {
-      this.donations = data; // Присвоюємо тільки масив донатів до локальної змінної
+      console.error('Error:', error);
+      return;
     }
+  
+    this.donations = await Promise.all(
+      data.map(async (donation: any) => {
+        try {
+          let shelterName = 'Unknown';
+  
+          if (donation.shelter_id) {
+            const shelter = await this.supabaseService.getShelterById(donation.shelter_id);
+            shelterName = shelter?.shelter_name || 'Unknown';
+  
+          } else if (donation.need_id) {
+            const need = await this.supabaseService.getNeedById(donation.need_id);
+            const animalId = need?.animal_id;
+  
+            if (animalId) {
+              const animal = await this.supabaseService.getAnimalById(animalId);
+              const shelter = await this.supabaseService.getShelterById(animal?.shelter_id);
+              shelterName = shelter?.shelter_name || 'Unknown';
+            }
+          }
+  
+          return {
+            ...donation,
+            shelter_name: shelterName,
+          };
+        } catch (e) {
+          console.warn('Error:', e);
+          return {
+            ...donation,
+            shelter_name: 'Unknown',
+          };
+        }
+      })
+    );
   }
 
-  goDetailedInfo() {
-    this.router.navigate(['/detailed-info']);
+  goDetailedInfo(donation_id: string) {
+    this.router.navigate(['/detailed-info', donation_id]);
   }
 
   goHomepage() {
